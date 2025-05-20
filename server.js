b864ca3cc695d49f0c505af40f1cc9a6095c3116
Bible's MacBook Pro <comim_it02@COMIM-IT02ui-MacBookPro.local>
2025-05-20 13:53:47 +0900

    캐시추가

--- server.js
+++ server.js
@@ -13,13 +13,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const targetURL = 'https://www.yakup.com/search/index.html?csearch_word=%ED%95%AD%EC%95%94&csearch_type=news';
 
+
+// 메모리 캐시 변수
+let cache = {
+  timestamp: 0,
+  data: null,
+};
+
+const CACHE_DURATION = 24 * 60 * 60 * 1000; // 5분
+
+
 app.get('/api/news', async (req, res) => {
+  const now = Date.now();
+
+  if (cache.data && now - cache.timestamp < CACHE_DURATION) {
+    return res.json(cache.data);
+  }
+
   try {
     const { data } = await axios.get(targetURL);
     const $ = cheerio.load(data);
 
     const newsList = [];
-    $('.info_con li').each((_, el) => {
+    $(".info_con li").each((_, el) => {
       const title = $(el).find(".title_con span").text().trim();
       const date = $(el).find(".date").text().trim();
       const url = "https://www.yakup.com" + $(el).find("a").attr("href");
@@ -30,6 +46,12 @@ app.get('/api/news', async (req, res) => {
 
       // console.log(title, urlAddr, date);
     });
+
+    // 캐시 저장
+    cache = {
+      timestamp: now,
+      data: newsList,
+    };
 
     res.json(newsList);
   } catch (err) {
